75bbc7443dabd3961b9f0b89f90acd2705c34fee
dlarson17 <dlarson17@my.whitworth.edu>
2014-12-04 13:39:02 -0800

    Options (Menu) COMPLETE!!!

--- Final Project/Final Project/Dialogue.h
+++ Final Project/Final Project/Dialogue.h
@@ -17,13 +17,6 @@ public:
 
 };
 
-class Menu {
-private: 
-	string a, b, c, d;
-
-public:
-	char menu(char choice);
-};
 
 void Welcome::welcome() {
 	string name; 
@@ -34,22 +27,6 @@ void Welcome::welcome() {
 	Sleep(1500);
 
 	cout << "You wake up in an abandoned space station and you don't remember how or why you are there. Find the bridge to figure out what happened..." << endl;
-}
-
-char Menu::char menu(char choice) {
-	do {
-		cout << "1) Go Straight." << endl;
-		cout << "2) Go Left." << endl;
-		cout << "3) Go Right." << endl;
-		cout << "4) Stay Put." << endl;
-		cin >> choice;
-		cout << endl << endl;
-		if (choice > '4' || choice < '1'){
-			cout << "That's not an option soldier!" << endl << endl;
-		}
-
-	} while (choice > '4' || choice < '1');
-	return choice;
 }
 
 
--- Final Project/Final Project/Source.cpp
+++ Final Project/Final Project/Source.cpp
@@ -4,14 +4,32 @@
 #include <ctime>
 #include "Dialogue.h"
 
+
+char menu(char choice);
+
 int main()
 {
-	
+	char choice;
 
 	Welcome w;
 	w.welcome();
+	
+	do {
+		cout << "1) Go Straight." << endl;
+		cout << "2) Go Left." << endl;
+		cout << "3) Go Right." << endl;
+		cout << "4) Stay Put." << endl;
+		cin >> choice;
+		cout << endl << endl;
+		if (choice > '4' || choice < '1'){
+			cout << "That's not an option soldier!" << endl << endl;
+		}
 
-	Menu m;
+	} while (choice > '4' || choice < '1');
+	return choice;
+
+	
+	
 
 
 
