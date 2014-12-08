cfe908c5cfdf2c81b3ddd8c6a50c60c451cb74e5
Drew Sears <asears17@my.whitworth.edu>
2014-12-07 20:53:47 -0800

    Functions
    All My code for functions

---
+++ Project Trial Run/Project Trial Run/main.cpp
@@ -0,0 +1,120 @@
+//
+//  main.cpp
+//  Project Trial Run
+//
+//  Created by Drew Sears on 12/5/14.
+//  Copyright (c) 2014 Drew Sears. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+    int choice;
+    
+    cout << "Choose what you want 1-4" << endl;
+    cin >> choice;
+    
+    switch (choice) {
+        case '1'://stayput
+            //death();
+            break;
+            
+        case '2': //left
+            //YN();
+            
+            if (1) {
+                //dialogue();
+            }
+            else
+                //= case 3
+            
+            break;
+            
+        case '3': //straight
+            //dialogueblaster();
+             //YNBlaster()
+            
+            if (1) {
+                //dialogueAlien();
+                
+                //YNFightorRun();
+                
+                if (1) {
+                    //deathAlien();
+                }
+                else
+                    //deathAlien();
+            }
+            
+            if (2) {
+                //dialogueLift();
+                
+                //YNLift();
+                
+                if (2) {
+                   //dialogue();
+                    //alienDeath();
+                }
+                if (1) {
+                    //dialogueLift();
+                    //dialoguebridge();
+                    //dialogueComputer();
+                    //findEscapePod();
+                    
+                    if (1) {
+                        //deathByLift()
+                    }
+                    if (2) {
+                        //dialogueSeeAlien();
+                        //YNFightFlight
+                        
+                        if (1) {
+                            //fight
+                            //deathbyAlien();
+                        }
+                        if (2) {
+                            //flight
+                            //dialogue()?
+                            //LRChoice()
+                            if (2) {
+                                //deathbyAlien?()
+                            }
+                            if (2) {
+                                //WoundedSoldierDialogue();
+                                //YNPickupsoldier()
+                                if (1) {
+                                    //No
+                                    //Death
+                                }
+                                if (2) {
+                                    //Yes
+                                    //Survive
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            
+            break;
+            
+            case '4': //Right
+            //YNThroughDoor();
+            
+            if (1) {
+                
+                
+            }
+            
+            
+            
+            
+        default:
+            break;
+    }
+    
+   
+}
--- Sears/Sears/main.cpp
+++ Sears/Sears/main.cpp
@@ -9,7 +9,34 @@
 
 #include <iostream>
 using namespace std;
+void death();
+void stay();
+void forward();
 int main() {
     
+    int user;
+    cout << "Welcome to Alien vs You" << endl;
+    cout << "Press 1: to stay put, 2 to move";
+    if (user == '1') {
+        death();
+    }
+    if (user== '2')
+        forward();
    
+}
+
+void death()
+{
+    cout << "You die" << endl;
+}
+
+void stay()
+{
+    cout << "You stay" << endl;
+    death();
+}
+
+void forward()
+{
+    cout << "You move forward";
 }
